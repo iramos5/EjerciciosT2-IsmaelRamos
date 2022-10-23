4e7d588855c8bf038a59f03972b6955751639379
iramos5 <ismael.ramos@iesnervion.es>
2022-10-23 15:51:09 +0200

    vale

--- EjerciciosT2-IsmaelRamos/src/ejercicios/Ejercicio01.java
+++ EjerciciosT2-IsmaelRamos/src/ejercicios/Ejercicio01.java
@@ -1,10 +1,19 @@
 package ejercicios;
+import java.util.Scanner;
 
 public class Ejercicio01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("prueba");
+		/* Escribe una aplicación que solicite al usuario un número comprendido entre 0 y 9999. 
+		 * La aplicación tendrá que indicar si el número introducido es capicúa.
+		 */
+
+		//variables
+		int num;
+		
+		//introducir datos
+		System.out.println("Introduce un número entre 0 y 9999: ");
+		Scanner scan = new Scanner (System.in);
 	}
 
 }
