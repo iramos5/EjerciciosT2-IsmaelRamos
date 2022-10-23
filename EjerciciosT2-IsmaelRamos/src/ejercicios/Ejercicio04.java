ce6d22c7ca9bcca34c02466d215596b89a9a18ff
iramos5 <ismael.ramos@iesnervion.es>
2022-10-23 16:15:02 +0200

    investigar también

---
+++ EjerciciosT2-IsmaelRamos/src/ejercicios/Ejercicio04.java
@@ -0,0 +1,24 @@
+package ejercicios;
+import java.util.Scanner;
+
+public class Ejercicio04 {
+
+	public static void main(String[] args) {
+		/* Escribe un programa que solicite al usuario un número entero entre 1 y 99. 
+		 * El programa debe mostrarlo con letras, por ejemplo, para 56 se verá: “cincuenta y seis”.
+		 */
+
+		//variables
+		Scanner scan = new Scanner (System.in); //inicio del escáner
+		int num;
+		
+		//introducir datos
+		System.out.println("Introduce un número entre 1 y 99: ");
+		num = scan.nextInt();
+		
+		//mostrar con letras
+		
+
+	}
+
+}
