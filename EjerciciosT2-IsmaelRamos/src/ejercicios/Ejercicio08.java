1fa768f735294589c0a208ae1515108765a875ea
iramos <iramos@106-24.info2>
2022-10-24 08:38:40 +0200

    ejercicio 8 terminado

--- EjerciciosT2-IsmaelRamos/src/ejercicios/Ejercicio08.java
+++ EjerciciosT2-IsmaelRamos/src/ejercicios/Ejercicio08.java
@@ -12,8 +12,29 @@ public class Ejercicio08 {
 		
 		//variables
 		Scanner scan = new Scanner (System.in); //iniciar escaner
-		 //variables
+		double precioKm=2.5, distancia, precioBillete, descuento, precioDescuento; //variables
+		int estancia; //variable
+		
+		//solicitar datos
+		System.out.println("Introduce la distancia a recorrer: "); //pedir distancia
+		distancia = scan.nextDouble(); //guardar distancia
+		System.out.println("Introduce el número de días de estancia: "); //pedir dias estancia
+		estancia = scan.nextInt(); //guardar estancia
+		
+		//calcular precio billete
+		
+		if (estancia > 7 && distancia > 800) { //si la estancia es mayor a 7 dias y la distancia mas de 800 km
+			precioBillete = precioKm*distancia; // cálculo de precio billete sin descuento
+			descuento = (precioBillete * 30)/100; // descuento que se aplica
+			precioDescuento = precioBillete - descuento; // precio final con descuento
+			System.out.println("El billete de tren le costaría " +precioDescuento+"€"); //respuesta al usuario
+		}
+		else {
+			precioBillete = precioKm*distancia; // cálculo de precio billete sin descuento
+			System.out.println("El billete de tren le costaría " +precioBillete+"€"); //respuesta al usuario
+		}
 
+		scan.close(); //cierre escaner
 
 	}
 
