92073956a03b409897f74da1b4601f224cfd784f
iramos5 <ismael.ramos@iesnervion.es>
2022-10-24 18:23:28 +0200

    Ejercicio 5 completo

--- EjerciciosT2-IsmaelRamos/src/ejercicios/Ejercicio05.java
+++ EjerciciosT2-IsmaelRamos/src/ejercicios/Ejercicio05.java
@@ -1,12 +1,34 @@
 package ejercicios;
 import java.util.Scanner;
 
+/*pruebas
+ * numero: 5 --> El valor absoluto de 5 es: 5
+ * numero: -5 --> El valor absoluto de -5 es: 5
+ */
+
 public class Ejercicio05 {
 
 	public static void main(String[] args) {
 		// Utiliza un operador ternario para calcular el valor absoluto de un número que se solicita al usuario por teclado.
 
-
+		//variables
+		Scanner scan = new Scanner (System.in); //inicio del escaner
+		int num, vAbs; //num: numero que se solicita al usuario por teclado y vAbs: valor absoluto
+		
+		//pedir numero
+		System.out.println("Introduce un numero: "); //pedir numero
+		num = scan.nextInt(); //guardar numero
+		
+		//calcular valor absoluto
+		vAbs = num > 0 ? num : -num; // el valor absoluto del numero
+		
+		//mostrar respuesta
+		System.out.println("El valor absoluto de " +num+ " es: "+vAbs); //mostrar solucion
+		
+		scan.close(); //cierre escaner
 	}
+	
+	
+	
 
 }
