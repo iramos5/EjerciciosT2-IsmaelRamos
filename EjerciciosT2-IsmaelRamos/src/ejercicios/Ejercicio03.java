6af53ff983abe6d0d2c1da7ed93477a19644bf41
iramos5 <ismael.ramos@iesnervion.es>
2022-10-24 19:29:34 +0200

    Ejercicio 3 completo

--- EjerciciosT2-IsmaelRamos/src/ejercicios/Ejercicio03.java
+++ EjerciciosT2-IsmaelRamos/src/ejercicios/Ejercicio03.java
@@ -1,6 +1,13 @@
 package ejercicios;
 import java.util.Scanner;
 
+/*pruebas
+ * comidaComprada = 10, numAnimales = 5, kilosTotal = 50 --> No disponemos de comida para todos los animales. De los 10kg de comida que hemos comprado, le corresponden 2kg a cada animal
+ * comidaComprada = 10, numAnimales = 0, kilosTotal = 50 --> No puede haber 0 animales
+ * comidaComprada = 10, numAnimales = 5, kilosTotal = 10 --> Disponemos de comida suficiente para los animales
+ */
+
+
 public class Ejercicio03 {
 
 	public static void main(String[] args) {
@@ -14,26 +21,34 @@ public class Ejercicio03 {
 		
 		//variables
 		Scanner scan = new Scanner (System.in); //inicio del escáner
-		int cantComida, numAnimales, kilosTotal, comidaDisponible;
+		int comidaComprada, numAnimales, kilosTotal, racion; /*variables. comidaComprada es la comida comprada, numAnimales el numero de animales, 
+		kilosTotal los kilos que comen en total los animales y racion lo que le corresponderia a cada animal si hubiera de menos
+		*/
 		
 		//pedir datos
-		System.out.println("Introduce la cantidad de comida comprada: ");
-		cantComida = scan.nextInt();
-		System.out.println("Introduce el número de animales: ");
-		numAnimales = scan.nextInt();
-		System.out.println("Introduce los kilos que comen los animales en total: ");
-		kilosTotal = scan.nextInt();
+		System.out.println("Introduce la cantidad de comida comprada: "); //pedir comida comprada
+		comidaComprada = scan.nextInt(); //guardar comida comprada
+		System.out.println("Introduce el número de animales: "); //pedir numero de animales
+		numAnimales = scan.nextInt(); //guardar numero de animales
+		System.out.println("Introduce los kilos que comen los animales en total: "); //pedir kilos que comen en total
+		kilosTotal = scan.nextInt(); //guardar kilos que comen en total
 		
 		//disponemos de comida?
-		comidaDisponible= cantComida / numAnimales;
-		kilosTotal = numAnimales;
+		if(numAnimales==0) { //si hay cero animales
+			System.out.println("No puede haber 0 animales"); //respuesta
+		}
 		
-		if (comidaDisponible >= kilosTotal) {
-			
+		else if (comidaComprada >= kilosTotal) { //sino si la comida comprada es mayor o igual a los kilos necesarios
+			System.out.println("Disponemos de comida suficiente para los animales"); //respuesta
+		}
+		else if (comidaComprada < kilosTotal) { //sino si la comida comprada es menor a los kilos necesarios
+			System.out.println("No disponemos de comida para todos los animales"); //respuesta
+			racion = comidaComprada/numAnimales; // se calcula la racion correspondiente para cada animal 
+			System.out.println("De los "+comidaComprada+"kg de comida que hemos comprado, le corresponden "+racion+"kg a cada animal");//respuesta
 		}
 		
 		
-		
+		scan.close(); // cierre escaner
 		
 	}
 
